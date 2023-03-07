287e48e3391a8aa6cca88df8b3a7e2d036bf0715
gytide <gytide@qq.com>
2023-03-07 12:48:53 +0800

    添加 dsrt app

--- dsrt2/dsrt2/settings.py
+++ dsrt2/dsrt2/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #add app
+    'rest_framework',
+    'dsrt'
 ]
 
 MIDDLEWARE = [
